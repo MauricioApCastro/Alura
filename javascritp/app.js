cb7eb6813151b3d10b7a6a358d18417a261d04da
MauricioApCastro <castromac6367@gmail.com>
2024-05-18 08:16:46 -0300

    numero de tentativas

--- javascritp/app.js
+++ javascritp/app.js
@@ -1,17 +1,19 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
 console.log(numeroSecreto)
-let chute ;
+let chute;
+let count = 0;
 
-while(chute != numeroSecreto)
-    {
-        chute = prompt('Escolha um número entre 1 e 10');
-        if (chute > numeroSecreto) {
-            alert('o número secreto é menor')
-        }
-    
-        else if(chute < numeroSecreto){
-            alert('o número secreto é maior')
-        }
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute > numeroSecreto) {
+        alert('o número secreto é menor')
+    }
+
+    else if (chute < numeroSecreto) {
+        alert('o número secreto é maior')
+    }
+    count++;
 }
 alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+alert(`Você tentou ${count} vezes!`);
