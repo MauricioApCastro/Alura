d69af04f3e6489b2b989d7a9d9cad8bb421d380d
mauricio <castromac6367@gmail.com>
2024-05-20 17:14:08 -0300

    função gerarNumeroAleatorio

--- javascritp/app.js
+++ javascritp/app.js
@@ -1,13 +1,20 @@
 
+let numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio(){
+  return parseInt (Math.random() * 10) + 1;
+  
+}
 
 function exibirMensagem(tag,texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+
 exibirMensagem('h1','Jogo do número secreto');
 exibirMensagem('p','Escolha um número entre 1 e 10');
 
 function verificarChute(){
-    console.log('O botão foi acionado');
+    console.log(numeroSecreto);
 }
 
