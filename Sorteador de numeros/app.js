2b3ec40907e9782425c93a66e08450616ea2f87f
MauricioApCastro <castromac6367@gmail.com>
2024-05-21 20:41:41 -0300

    inicio do projeto

---
+++ Sorteador de numeros/app.js
@@ -0,0 +1,6 @@
+function sortear(){
+let quantidade = document.getElementById("quantidade");
+let de = document.getElementById("de");
+let ate = document.getElementById("ate");
+
+}
--- javascritp/app.js
+++ javascritp/app.js
@@ -1,20 +1,38 @@
 
 let numeroSecreto = gerarNumeroAleatorio();
 
-function gerarNumeroAleatorio(){
-  return parseInt (Math.random() * 10) + 1;
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10) + 1;
 }
 
-function exibirMensagem(tag,texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+function exibirMensagem(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 }
 
-exibirMensagem('h1','Jogo do número secreto');
-exibirMensagem('p','Escolha um número entre 1 e 10');
+exibirMensagem('h1', 'Jogo do número secreto');
+exibirMensagem('p', 'Escolha um número entre 1 e 10');
 
-function verificarChute(){
-    let chute = document.querySelector('input').value;
-    console.log(numeroSecreto == chute);
+
+
+function verificarChute() {
+  let chute = document.querySelector('input').value;
+
+  if (chute == numeroSecreto) {
+    let tentativa = 1;
+
+    exibirMensagem('h1', 'Acertou!');
+    exibirMensagem('p', 'Você descobriu o número secreto com 1 tentativa!');
+  } else {
+    if (chute > numeroSecreto) {
+      exibirMensagem('p', 'O número secreto é menor');
+    } else {
+      exibirMensagem('p', 'O número secreto é maior');
+    }
+  }
 }
+
+
+
+
 
