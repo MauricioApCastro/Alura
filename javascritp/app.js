8112d4c97cc9d9f4786dbfe8971aafa34add8cbd
mauricio <castromac6367@gmail.com>
2024-05-21 07:50:28 -0300

    mostrar acerto ou erro do usuário

--- javascritp/app.js
+++ javascritp/app.js
@@ -1,20 +1,30 @@
 
-let numeroSecreto = gerarNumeroAleatorio();
+//let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = 5;
 
-function gerarNumeroAleatorio(){
-  return parseInt (Math.random() * 10) + 1;
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10) + 1;
 }
 
-function exibirMensagem(tag,texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+function exibirMensagem(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 }
 
-exibirMensagem('h1','Jogo do número secreto');
-exibirMensagem('p','Escolha um número entre 1 e 10');
+exibirMensagem('h1', 'Jogo do número secreto');
+exibirMensagem('p', 'Escolha um número entre 1 e 10');
 
-function verificarChute(){
-    let chute = document.querySelector('input').value;
-    console.log(numeroSecreto == chute);
+function verificarChute() {
+  let chute = document.querySelector('input').value;
+  if (chute == numeroSecreto) {
+    exibirMensagem('p', 'Você acertou!');
+  } else {
+    if (chute > numeroSecreto) {
+      exibirMensagem('p', 'o número é menor!');
+    } else {
+      exibirMensagem('p', 'o número é maior!');
+    }
+  }
 }
 
