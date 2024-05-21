b1673a5b0aa91dc1fcbdb813b58c8dbf52e82c42
MauricioApCastro <castromac6367@gmail.com>
2024-05-21 20:59:49 -0300

    recuperando dados da pagina

--- Sorteador de numeros/app.js
+++ Sorteador de numeros/app.js
@@ -3,4 +3,9 @@ let quantidade = document.getElementById('quantidade').value;
 let de = document.getElementById('de').value;
 let ate = document.getElementById('ate').value;
 
+alert("" + quantidade);
+alert("" + de);
+alert("" + ate);
+
+}
 
