2639dbe7bf410d17b4d6b97765c932dcec32bc57
mauricio <castromac6367@gmail.com>
2024-05-22 08:58:10 -0300

    tratando a duplicidade de números sorteados

--- Sorteador de numeros/app.js
+++ Sorteador de numeros/app.js
@@ -1,22 +1,27 @@
 //recupera os inputs
-function sortear(){
+function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    
+
     let sorteados = [];
     let numero;
-    
+
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
+
+        //tratando a duplicidade de números sorteados
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
         sorteados.push(numero);
     }
-    
-   
-let resultado = document.getElementById('resultado');
-resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
 
 }
 function obterNumeroAleatorio(min, max) {
-return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }   
