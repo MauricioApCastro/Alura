4c0c81a4e90cb8a26f812373ee7d07e7ff5fffae
MauricioApCastro <castromac6367@gmail.com>
2024-05-23 19:33:07 -0300

    gera número aleatório

--- Sorteador de numeros/app.js
+++ Sorteador de numeros/app.js
@@ -1,6 +1,24 @@
+let numero = obterNumeroAleatorio(de, ate);
+
 function sortear(){
-    let quantidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
-    let ate = document.getElementById("ate").value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
     
+    let sorteados = [];
+    let numero;
+    
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+    
+    alert(sorteados);
 }
+
+
+//gera número aleatório
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
