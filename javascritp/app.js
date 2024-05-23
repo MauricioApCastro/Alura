b954ca6478fe52fef1fb8c3db1d6893a6c00e614
mauricio <castromac6367@gmail.com>
2024-05-23 12:05:47 -0300

    reinicia e desabilita o botao

--- javascritp/app.js
+++ javascritp/app.js
@@ -1,11 +1,16 @@
 
-ExibirTextoNaTela('h1', 'Jogo do número secreto');
+MensagemInicial();
+function MensagemInicial(){
+  ExibirTextoNaTela('h1', 'Jogo do número secreto');
 ExibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+}
+
 
 //insere frases na tela
 function ExibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
+
 }
 
 //gera numero aleatorio de 1 a 10
@@ -18,14 +23,14 @@ let i = 1;
 //gera o resultado
 function verificarChute() {
   let chute = document.querySelector('input').value;
-  
+
   if (chute == numeroSecreto) {
-    let tentativa = i == 1 ? "tentativa":"tentativas";
+    let tentativa = i == 1 ? "tentativa" : "tentativas";
     ExibirTextoNaTela('h1', `você acertou em ${i} ${tentativa}`);
     ExibirTextoNaTela('p', 'Parabéns');
-document.getElementById('reiniciar').removeAttribute('disabled');
+    //habilita botão
+    document.getElementById('reiniciar').removeAttribute('disabled');
 
-    
   } else {
     if (chute > numeroSecreto) {
       ExibirTextoNaTela('p', 'O número é menor');
@@ -38,7 +43,19 @@ document.getElementById('reiniciar').removeAttribute('disabled');
 
 }
 
-function limpaCampo(){
+function limpaCampo() {
   chute = document.querySelector('input');
   chute.value = '';
+}
+
+function reiniciarJogo(){
+  limpaCampo();
+  numeroAleatorio();
+  MensagemInicial();
+
+  i = 1;
+  //reinicia e desabilita o botao
+  document.getElementById('reiniciar').setAttribute('disabled',true);
+
+
 }
