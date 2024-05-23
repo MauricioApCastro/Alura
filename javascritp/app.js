03aaccb66202a14c7abd2f9e590d4139bedee15a
MauricioApCastro <castromac6367@gmail.com>
2024-05-22 23:22:26 -0300

    refatoraçao

--- javascritp/app.js
+++ javascritp/app.js
@@ -1,56 +1,51 @@
+let numeroSorteado = 5;
+let i = 1;
+//mostrar mensagem na tela
+mostrarMensagem('h1', 'Jogo do número secreto');
+mostrarMensagem('p', 'Escolha um número de 1 a 10');
 
-//let numeroSecreto = gerarNumeroAleatorio();
-let numeroSecreto = 5;
-let tentativa = 1;
-
-ExibirMensagemTelaInicial()
+//gerar mensagem na tela
+function mostrarMensagem(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+//gerar numero sorteador de 1 a 10
 function gerarNumeroAleatorio() {
-  return parseInt(Math.random() * 10) + 1;
+    return parseInt(Math.random() * 10) + 1;
 }
 
-function exibirMensagem(tag, texto) {
-  let campo = document.querySelector(tag);
-  campo.innerHTML = texto;
-}
-function ExibirMensagemTelaInicial() {
-  exibirMensagem('h1', 'Jogo do número secreto');
-  exibirMensagem('p', 'Escolha um número entre 1 e 10!');
-}
+function MostrarResultado() {
+    let chute = document.querySelector('input').value;
+    let palavraTentativa = i == 1 ? "tentativa" : "tentativas";
+    if (chute == numeroSorteado) {
+        mostrarMensagem('h1', 'Parabéns !!');
+        mostrarMensagem('p',` Você acertou com ${i} ${palavraTentativa}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSorteado) {
+            mostrarMensagem('p', 'o número sorteado é menor !!');
+        } else {
+            mostrarMensagem('p', 'o número sorteado é maior !!');
+        }
+        i++;
+        limpaCampo();
+    }
 
+}
 
 function verificarChute() {
-  let chute = document.querySelector('input').value;
-
-  if (chute == numeroSecreto) {
-    let palavraTentativa = tentativa == 1 ? 'tentativa' : ' tentativas';
-    exibirMensagem('p', ` Você descobriu o número secreto com ${tentativa} ${palavraTentativa}!`);
-    document.getElementById('reiniciar').removeAttribute('disabled');
-
-  } else {
-    if (chute > numeroSecreto) {
-      exibirMensagem('p', 'o número é menor!');
-    } else {
-      exibirMensagem('p', 'o número é maior!');
-    }
-    tentativa++;
-    limpaCampo();
-  }
-
+    MostrarResultado();
 }
-function limpaCampo() {
-  let limpa = document.querySelector('input');
-  limpa.value = "";
 
+function limpaCampo() {
+    let chute = document.querySelector('input');
+    chute.value = "";
+ 
 }
 function reiniciarJogo() {
-  numeroSecreto = gerarNumeroAleatorio();
-  limpaCampo();
-  ExibirMensagemTelaInicial()
-  tentativa = 1;
-  document.getElementById('reiniciar').setAttribute('disabled',true);
-}
-
-
-
-
-
+    numeroSecreto = gerarNumeroAleatorio();
+    limpaCampo();
+    ExibirMensagemTelaInicial();
+    tentativa = 1;
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+  }
