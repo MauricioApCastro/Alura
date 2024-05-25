6e03451e7f960d2701bd9298ee2f13a241d93ab7
MauricioApCastro <castromac6367@gmail.com>
2024-05-25 10:41:31 -0300

    fim do projeto

--- Sorteador de numeros/app.js
+++ Sorteador de numeros/app.js
@@ -1,13 +1,23 @@
 function sortear() {
-    //lê os inputs
+    // Lê os inputs
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
-    alert(numero);
 
+    // Verifica se os valores são válidos
+    if (isNaN(quantidade) || isNaN(de) || isNaN(ate) || quantidade <= 0 || de > ate) {
+        alert("Por favor, insira valores válidos.");
+        return;
+    }
+
+    // Chama a função para gerar números aleatórios
+    let numeros = gerarNumerosAleatorios(quantidade, de, ate);
+
+    // Exibe os números sorteados
+    document.getElementById("resultado").innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numeros.join(", ")}</label>`;
+
+    alteraStatusBotao();
 }
-// Chama a função para gerar números aleatórios
-let numeroAleatorio = gerarNumerosAleatorios(quantidade, de, ate);
 
 function gerarNumerosAleatorios(quantidade, de, ate) {
     let numeros = [];
@@ -16,4 +26,29 @@ function gerarNumerosAleatorios(quantidade, de, ate) {
         numeros.push(numero);
     }
     return numeros;
+}
+
+function reiniciar() {
+    // Limpa os inputs e o resultado
+    document.getElementById("quantidade").value = '';
+    document.getElementById("de").value = '';
+    document.getElementById("ate").value = '';
+    document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+
+
+    // Desabilita o botão de sortear
+    document.getElementById("btn-sortear").disabled = true; // Comente esta linha se não deseja desabilitar o botão de sortear
+    // Habilita o botão de reiniciar
+    document.getElementById("btn-reiniciar").disabled = false; // Comente esta linha se não deseja habilitar o botão de reiniciar
+}
+
+function alteraStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
 }
