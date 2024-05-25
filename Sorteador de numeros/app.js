b57c42b2ad48f7eceeb5c49fc485f1462576a490
MauricioApCastro <castromac6367@gmail.com>
2024-05-25 10:03:30 -0300

    // Chama a função para gerar números aleatórios

--- Sorteador de numeros/app.js
+++ Sorteador de numeros/app.js
@@ -1,11 +1,19 @@
-function sortear(){
+function sortear() {
     //lê os inputs
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
+    alert(numero);
 
-    alert("" + quantidade);
-    alert("" + de);
-    alert("" + ate);
+}
+// Chama a função para gerar números aleatórios
+let numeroAleatorio = gerarNumerosAleatorios(quantidade, de, ate);
 
+function gerarNumerosAleatorios(quantidade, de, ate) {
+    let numeros = [];
+    for (let i = 0; i < quantidade; i++) {
+        let numero = Math.floor(Math.random() * (ate - de + 1)) + de;
+        numeros.push(numero);
+    }
+    return numeros;
 }
