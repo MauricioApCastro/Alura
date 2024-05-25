22fbe58c314fd814b16e98c93eb9e8024e095974
MauricioApCastro <castromac6367@gmail.com>
2024-05-25 09:45:11 -0300

    lê os inputs

---
+++ Sorteador de numeros/app.js
@@ -0,0 +1,11 @@
+function sortear(){
+    //lê os inputs
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+
+    alert("" + quantidade);
+    alert("" + de);
+    alert("" + ate);
+
+}
