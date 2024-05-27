d297fd36072fae13985f27675239856eaebeaad5
mauricio <castromac6367@gmail.com>
2024-05-27 13:34:26 -0300

    fim do projeto

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -4,12 +4,29 @@ function alterarStatus(id) {
   let imgDiv = gameClicado.querySelector('.dashboard__item__img');
 
   if (botao.textContent === "Alugar") {
-      botao.textContent = "Devolver";
-      botao.classList.add('dashboard__item__button--return');
-      imgDiv.classList.add('dashboard__item__img--rented');
-  } else if (botao.textContent === "Devolver") {
+    botao.textContent = "Devolver";
+    botao.classList.add('dashboard__item__button--return');
+    imgDiv.classList.add('dashboard__item__img--rented');
+   
+  }
+  else if (botao.textContent === "Devolver") {
+    if (confirm("Você quer devolver?")) {
+      alert("Você escolheu devolver.");
       botao.textContent = "Alugar";
       botao.classList.remove('dashboard__item__button--return');
       imgDiv.classList.remove('dashboard__item__img--rented');
+    } else {
+      alert("Você escolheu não devolver.");
+      return;
+    }
   }
+  const totalJogosAlugados = contarJogosAlugados();
+  alert(`Total de jogos alugados: ${totalJogosAlugados}`);
+
 }
+function contarJogosAlugados() {
+  const jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented');
+  return jogosAlugados.length;
+
+}
+
