6d53fb2c465abe1f4846dac264a356bcfb77690d
mac <castromac6367@gmail.com>
2024-06-05 17:22:41 -0300

    // Loop para gerar números aleatórios

--- Sorteador de numeros/app.js
+++ Sorteador de numeros/app.js
@@ -1,12 +1,22 @@
-//recuperando os dados
-function sortear(){
-let quantidade = document.getElementById("quantidade").value;
-let de = document.getElementById("de").value;
-let ate = document.getElementById("ate").value;
-alert(quantidade);
+// Função para recuperar os dados e sortear números aleatórios
+function sortear() {
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
 
+    let listaNumero = [];
+
+    // Loop para gerar números aleatórios
+    for (let i = 0; i < quantidade; i++) {
+        let numeroAleatorio = gerarNumeroAleatorio(de, ate);
+        listaNumero.push(numeroAleatorio);
+    }
+
+    alert(listaNumero);
 }
-function gerarNumeroAleatorio(){
-    
+
+// Função que gera um número aleatório
+function gerarNumeroAleatorio(minimo, maximo) {
+    return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
 }
 
