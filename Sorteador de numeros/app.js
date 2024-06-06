397b625729e6e1819764ebffaf6eba07d532c91c
MauricioApCastro <castromac6367@gmail.com>
2024-06-05 21:35:44 -0300

    funcao reiniciar

--- Sorteador de numeros/app.js
+++ Sorteador de numeros/app.js
@@ -1,9 +1,10 @@
+let botao;
 // Função para recuperar os dados e sortear números aleatórios
 function sortear() {
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
-
+     botao = document.getElementById("btn-reiniciar");
     let listaNumero = [];
 
     // Loop para gerar números aleatórios
@@ -11,13 +12,13 @@ function sortear() {
         let numeroAleatorio = gerarNumeroAleatorio(de, ate);
         listaNumero.push(numeroAleatorio);
     }
-//mostra resultado na tela
-let resultado = document.getElementById("resultado");
-resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${listaNumero}</label>`;
+    //mostra resultado na tela
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${listaNumero}</label>`;
 
-    
+
     //habilita botão reiniciar
-    let botao = document.getElementById("btn-reiniciar");
+   
     botao.classList.remove("container__botao-desabilitado");
     botao.classList.add("container__botao");
 }
@@ -26,4 +27,13 @@ resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${lis
 function gerarNumeroAleatorio(minimo, maximo) {
     return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
 }
+//funcao reiniciar
+function reiniciar() {
+    quantidade.value = "";
+    de.value = "";
+    ate.value = "";
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
+    botao.classList.remove("container__botao");
+    botao.classList.add("container__botao-desabilitado");
 
+}
