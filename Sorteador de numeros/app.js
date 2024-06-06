9bb53092cfbd824ebb1fe012d4acd8b6948c85de
MauricioApCastro <castromac6367@gmail.com>
2024-06-05 22:02:26 -0300

    //evita a duplicidade

--- Sorteador de numeros/app.js
+++ Sorteador de numeros/app.js
@@ -4,21 +4,25 @@ function sortear() {
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
-     botao = document.getElementById("btn-reiniciar");
+    botao = document.getElementById("btn-reiniciar");
     let listaNumero = [];
 
-    // Loop para gerar números aleatórios
-    for (let i = 0; i < quantidade; i++) {
-        let numeroAleatorio = gerarNumeroAleatorio(de, ate);
-        listaNumero.push(numeroAleatorio);
-    }
+   // Loop para gerar números aleatórios
+   for (let i = 0; i < quantidade; i++) {
+    let numeroAleatorio;
+    do {
+        numeroAleatorio = gerarNumeroAleatorio(de, ate);
+        //evita a duplicidade
+    } while (listaNumero.includes(numeroAleatorio));
+    listaNumero.push(numeroAleatorio);
+}
     //mostra resultado na tela
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${listaNumero}</label>`;
 
 
     //habilita botão reiniciar
-   
+
     botao.classList.remove("container__botao-desabilitado");
     botao.classList.add("container__botao");
 }
