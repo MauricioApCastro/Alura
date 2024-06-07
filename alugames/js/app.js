ce301b02e180350ee1261ba04362088c322148d4
MauricioApCastro <castromac6367@gmail.com>
2024-06-06 22:02:25 -0300

    recuperando os dados da tela

---
+++ alugames/js/app.js
@@ -0,0 +1,10 @@
+function alterarStatus(id){
+let elemento = document.getElementById(`id-${id}`);
+let imagem = document.querySelector(".dashboard__item__img");
+let botao = document.querySelector(" .dashboard__item__button");
+
+
+
+
+
+}
