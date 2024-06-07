15d4d26731aad7590be1acdb9158534686c13a27
MauricioApCastro <castromac6367@gmail.com>
2024-06-06 23:21:23 -0300

    contador de  jogos alugados

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,19 +1,41 @@
+let jogosAlugados = 0;
+
+
+function contarEExibirJogosAlugados() {
+    if (jogosAlugados == '0') {
+        alert('Todos os jogos estão disponíveis');
+    } else {
+        alert(`Total de jogos alugados: ${jogosAlugados}`);
+    }
+}
+
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        if (confirm("Deseja devolver o jogo?")) {
-            imagem.classList.remove('dashboard__item__img--rented');
-            botao.classList.remove('dashboard__item__button--return');
-            botao.textContent = 'Alugar';
-        }
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        jogosAlugados--;
     } else {
-
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
+        jogosAlugados++;
     }
 
+
+    contarEExibirJogosAlugados();
 }
+
+
+// Inicializa a contagem considerando que os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function () {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
