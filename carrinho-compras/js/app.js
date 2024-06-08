e6c5a6d9e89a04be7f7c1fdfacc1ea631e8c958b
MauricioApCastro <castromac6367@gmail.com>
2024-06-08 10:19:51 -0300

    somando itens do carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,17 +1,23 @@
-function adicionar() {
-    //recuperar os dados do formulário
-    let produto = document.getElementById('produto').value;
-    let quantidade = document.getElementById('quantidade').value;
-    let nomeProduto = produto.split('-')[0];
-    let ValorUnitario = produto.split('R$')[1];
+let total = 0;
 
-    //calculando o valor do produto
-    let preco = quantidade * ValorUnitario;
-    //adicionando produto no carrinho
-    let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML = carrinho.innerHTML+`<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${ValorUnitario}</span>
+function adicionar() {
+  //recuperar os dados do formulário
+  let produto = document.getElementById('produto').value;
+  let quantidade = document.getElementById('quantidade').value;
+  let nomeProduto = produto.split('-')[0];
+  let ValorUnitario = produto.split('R$')[1];
+
+
+  //calculando o valor do produto
+  let preco = quantidade * ValorUnitario;
+  //adicionando produto no carrinho
+  let carrinho = document.getElementById('lista-produtos');
+  carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`;
 
+  let valorTotal = document.getElementById('valor-total').value;
+  total = total + preco;
+  alert(total);
 
 }
