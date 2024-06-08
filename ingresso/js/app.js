1a2c37cbbf2a984cf2154c5dce5cd7176b060c81
MauricioApCastro <castromac6367@gmail.com>
2024-06-08 11:52:22 -0300

    recuperação dos dados

---
+++ ingresso/js/app.js
@@ -0,0 +1,7 @@
+function comprar(){
+   let tipoIngresso = document.getElementById('tipo-ingresso').value;
+   let quantidade = document.getElementById('qtd').value;
+   alert(tipoIngresso);
+   alert(quantidade);
+   
+}
