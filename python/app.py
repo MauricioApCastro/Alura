ce5932ee8249e682e5b1278dbd4d3ec63a08ea1e
MauricioApCastro <castromac6367@gmail.com>
2024-06-20 22:34:51 -0300

    #input da opçao

--- python/app.py
+++ python/app.py
@@ -5,4 +5,7 @@ print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair')
+#input da opçao
+opcao_escolhida = input("Escolha uma opção: ")
+print("Você escolheu a opção {}".format(opcao_escolhida))
 
