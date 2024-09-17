1fc8219de6772ce079ff2651df9261d6ba52428a
mac <castromac6367@gmail.com>
2024-09-17 16:16:44 -0300

    propriedades

--- ScreenSound/ScreenSound/Musica.cs
+++ ScreenSound/ScreenSound/Musica.cs
@@ -1,22 +1,22 @@
 ﻿public class Musica
 {
-    public string artista;
-    public int duracao;
-    public string nome;
-    private bool assinatura;
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public string Nome { get; set; }
+    private bool Assinatura { get; set; }
 
    
     public void EscreveDisponivel(bool valor)
     {
-      assinatura = valor;
+      Assinatura = valor;
     }
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome:{nome}");
-        Console.WriteLine($"Artista:{nome}");
-        Console.WriteLine($"Duração:{nome}");
-        if (assinatura) { Console.WriteLine("Disponível no plano"); }
+        Console.WriteLine($"Nome:{Nome}");
+        Console.WriteLine($"Artista:{Nome}");
+        Console.WriteLine($"Duração:{Nome}");
+        if (Assinatura) { Console.WriteLine("Disponível no plano"); }
         else
         {
             Console.WriteLine("Adquira o plano Plus+");
--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,15 +1,15 @@
 ﻿Musica musica1 = new Musica();
 
-musica1.artista = "The Police";
-musica1.nome = "Roxanne";
-musica1.duracao = 273;
+musica1.Artista = "The Police";
+musica1.Nome = "Roxanne";
+musica1.Duracao = 273;
 musica1.EscreveDisponivel(true);
 
 Musica musica2 = new Musica();
 
-musica2.artista = "U2";
-musica2.nome = "Vertigo";
-musica2.duracao = 367;
+musica2.Artista = "U2";
+musica2.Nome = "Vertigo";
+musica2.Duracao = 367;
 musica2.EscreveDisponivel(false);
 
 musica1.ExibirFichaTecnica();
