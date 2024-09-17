fe0020bfbe00a51259880cf4d8961a2f2f698845
mac <castromac6367@gmail.com>
2024-09-17 16:46:29 -0300

    cria uma lista chamada musicas e adiciona pelo método

--- ScreenSound/ScreenSound/Album.cs
+++ ScreenSound/ScreenSound/Album.cs
@@ -1,11 +1,12 @@
 ﻿public class Album
 {
+    private List< Musica > musicas;
     public string Nome { get; set; }
     public int DuracaoTotal { get; set; }
 
     public void AdicionarMusica(Musica musica)
     {
-        
+        musicas.Add(musica);
     }
 
 }
