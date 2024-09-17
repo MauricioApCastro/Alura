156a15253a3e3b8fa71648e042338122e5f4230c
mac <castromac6367@gmail.com>
2024-09-17 15:59:55 -0300

    criando um método

--- ScreenSound/ScreenSound/Musica.cs
+++ ScreenSound/ScreenSound/Musica.cs
@@ -1,7 +1,21 @@
-﻿class Musica
+﻿public class Musica
 {
     public string artista;
     public int duracao;
     public string nome;
-    public bool disponivel;
+    public bool assinatura;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome:{nome}");
+        Console.WriteLine($"Artista:{nome}");
+        Console.WriteLine($"Duração:{nome}");
+        if (assinatura) { Console.WriteLine("Disponível no plano"); }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+
+    }
+
 }
--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Musica musica1 = new Musica();
+
+musica1.artista = "The Police";
+musica1.nome = "Roxanne";
+musica1.duracao = 273;
+musica1.assinatura = true;
+
+Musica musica2 = new Musica();
+
+musica2.artista = "U2";
+musica2.nome = "Vertigo";
+musica2.duracao = 367;
+musica2.assinatura = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+
+
+
+
+
